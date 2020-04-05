873f8ee3f25166e2fe78693cb0181d0397b61eef
tsemkaloalena <alenatsemkalo@gmail.com>
2020-04-05 15:55:06 +0300

    проверка адреса

--- data/jobs.py
+++ data/jobs.py
@@ -1,9 +1,11 @@
 import datetime
+
+import requests
 import sqlalchemy
 from flask_wtf import FlaskForm
 from sqlalchemy import orm
 from wtforms import StringField, TextAreaField, SubmitField, BooleanField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 from .db_session import SqlAlchemyBase
 
@@ -26,10 +28,27 @@ class Jobs(SqlAlchemyBase):
 
 
 class JobsForm(FlaskForm):
+    def check_address(self, field):
+        try:
+            geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+            geocoder_params = {
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                "geocode": field.data,
+                "format": "json"}
+            response = requests.get(geocoder_api_server, params=geocoder_params)
+            if response:
+                json_response = response.json()
+                toponym = json_response["response"]["GeoObjectCollection"][
+                    "featureMember"][0]["GeoObject"]["Point"]["pos"]
+            else:
+                raise ValidationError('invalid address')
+        except:
+            raise ValidationError('invalid address')
+
     description = StringField("Краткое описание работы", validators=[DataRequired()])
     employer = IntegerField("id нанимателя")
     salary = IntegerField("Оплата за услугу (в рублях)", validators=[DataRequired()])
-    address = TextAreaField("Адрес", validators=[DataRequired()])
+    address = TextAreaField("Адрес", validators=[DataRequired(), check_address])
     info = TextAreaField("Доп. информация")
     coords = TextAreaField("Координаты")
     date = TextAreaField("Когда нужно прийти (дата и время)", validators=[DataRequired()])
